dd6373ff5dbd4ba8272d01ea352012d522b81e57
prashant9191 <sonupv000@gmail.com>
2023-09-01 13:19:03 +0530

    fix bug

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import "./style.scss";
 import menu_icon from "../../Assets/Icons/menu_icon.png";
-import i1 from "../../Assets/Icons/4.png";
-import i2 from "../../Assets/Icons/5.png";
-import i3 from "../../Assets/Icons/3.png";
 import x from "../../Assets/Icons/x.png";
 import DIVESH from "./Divesh.pdf";
 
@@ -32,18 +29,33 @@ const Header = () => {
           <a href="#about">About Me</a>
           <a href="#work">My Projects</a>
           <a href="#contact">Talk To Me</a>
-          <a href={DIVESH} target="_blank" rel="noopener noreferrer" >
+          <a href={DIVESH} target="_blank" rel="noopener noreferrer">
             My Resume
           </a>
         </div>
         <div className="socialmedia_links">
-          <div onClick={()=>window.open("https://www.behance.net/divesh", '_blank')}>
+          <div
+            onClick={() =>
+              window.open("https://www.behance.net/divesh", "_blank")
+            }
+          >
             <i class="fab fa-behance"></i>
           </div>
-          <div  onClick={()=>window.open("https://www.linkedin.com/in/divesh-3327a0a7/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B3CBzAbnDRdOVT1xjYy%2FMvg%3D%3D", '_blank')}>
+          <div
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/divesh-3327a0a7/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B3CBzAbnDRdOVT1xjYy%2FMvg%3D%3D",
+                "_blank"
+              )
+            }
+          >
             <i class="fab fa-linkedin-in"></i>
           </div>
-          <div  onClick={()=>window.open("https://twitter.com/divesh29562085", '_blank')}>
+          <div
+            onClick={() =>
+              window.open("https://twitter.com/divesh29562085", "_blank")
+            }
+          >
             <i class="fab fa-twitter"></i>
           </div>
         </div>
